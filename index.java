c8c2092c7b382d9b1e3c592778ae297c6aeafc2a
Ahmed Ali <aali.97@yahoo.com>
2019-11-13 19:03:08 +0500

    Update index.java

--- index.java
+++ index.java
@@ -105,6 +105,7 @@ class MainActivity ...
   MainActivity to a fragment.
 */
 
+Purposes
 --- A fragment has its own layout and its own behaviour with its own life cycle callbacks.
 --- You can add or remove fragments in an activity while the activity is running.
 --- You can combine multiple fragments in a single activity to build a multi-pane UI.
@@ -135,6 +136,140 @@ class MainActivity ...
     }
 
 // ================================================================================================================ //
+/*
+  QUESTION - Write a code for a button click event that load a fragment named FirstFragment and send it a
+  numeric value “25”. Write the fragment code of MainActivity and the receiving code on
+  Fragment, where the received number is assigned to a textView defined in Fragment layout
+*/
+
+class MainActivity ... 
+  
+  public void btnClick(View view) {
+
+      FragmentManager manager = getSupportFragmentManager();
+      FragmentTransaction transaction = manager.beginTransaction();
+
+      FrameLayout frameLayout = findViewById(R.id.frame);
+      frameLayout.removeAllViews();
+
+      Fragment fragment = new FirstFragment();
+
+      Bundle bundle = new Bundle();
+      bundle.putInt("num", 25);
+      fragment.setArguments(bundle);
+
+      transaction.add(R.id.frame, fragment);
+
+      transaction.commit();
+
+    }
+
+class FirstFragment ... 
+  
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle) {
+
+      View view = inflater.inflate(R.layout.first_fragment, container, false);
+
+      TextView textView = view.findViewById(R.id.textView);
+      int num = getArguments().getInt("num");
+      textView.setText(String.valueOf(num));
+
+      return view;
+    }
+
+
+// ================================================================================================================ //
+/*
+  QUESTION - Write the purpose of fragments and write code of passing a string value “Pakistan” from
+  MainActivity to a fragment. Write also code in fragment that should concatenate “ Zindabad”
+  with Pakistan and return the whole string. In the MainActivity, the final string “Pakistan
+  Zindabad” should be received.
+*/
+
+Purposes
+--- A fragment has its own layout and its own behaviour with its own life cycle callbacks.
+--- You can add or remove fragments in an activity while the activity is running.
+--- You can combine multiple fragments in a single activity to build a multi-pane UI.
+--- A fragment can be used in multiple activities.
+--- A fragment can implement a behaviour that has no user interface component.
+
+
+class MainActivity ... 
+  
+    public void onCreate(Bundle savedInstanceState) {
+
+        if (getIntent() != null && getIntent().getExtras() != null) {
+            String value = getIntent().getExtras().getString("value");
+            TextView textView = findViewById(R.id.textView);
+            textView.setText("Value received : " + value);
+        }
+
+    }// onCreate ends here 
+
+    public void btnClick(View vew) {
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+
+        FrameLayout frameLayout = findViewById(R.id.frame);
+        frameLayout.removeAllViews();
+
+        Fragment fragment = new FirstFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putString("value", "Pakistan");
+        fragment.setArguments(bundle);
+
+        transaction.add(R.id.frame, fragment);
+
+        transaction.commit();
+
+     }// btnClick ends here 
+
+
+class FirstFragment ... 
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle) {
+
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+
+        String value = getArguments().getString("value");
+        value += " Zindabad";
+
+        Intent intent = new Intent(getContext(), MainActivity.class);
+        intent.putExtra("value", value);
+        getContext().startActivity(intent);
+
+        return view;
+    }
+
+
+// ================================================================================================================ //
+/*
+  QUESTION - Create an application in which the MainActivity passes an ID to a Fragment. The fragment
+  fetches the record of a user from SQLite database and loads it into TextViews defined in
+  Fragment’s layout file.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
